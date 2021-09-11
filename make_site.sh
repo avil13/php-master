d46a455e18d3e4819b035c63b248553ac64d9b18
avil13 <13.avil@gmail.com>
2021-09-11 18:37:39 +0300

    chore: translate make_script.sh

--- make_site.sh
+++ make_site.sh
@@ -6,9 +6,6 @@ off='\033[0m'             # Text Reset
 red='\033[0;31m'          # Red
 green='\033[0;32m'        # Green
 yellow='\033[0;33m'       # Yellow
-blue='\033[0;34m'         # Blue
-purple='\033[0;35m'       # Purple
-cyan='\033[0;36m'         # Cyan
 
 # # # # # # # # # # # # # # # # # #
 
@@ -19,26 +16,26 @@ NGINX="$ROOT/etc/nginx"
 SSL="$ROOT/etc/ssl"
 
 # TEXT
-DESCRIPTION="Это скрипт для добавления сайта. \
-\n- добавляет новую директорию в 'www'\
-\n- добавляет конфиг в 'etc/nginx/confg.d'\
-\n- создает локальный сертификат в 'etc/ssl'\n\n\
-Для начала работы введите название сайта например 'example.loc'\n"
+DESCRIPTION="This is a script for adding a site. \
+\n- Adds a new directory to the 'www'\
+\n- Adds config to 'etc/nginx/confg.d'\
+\n- Creates a local certificate in 'etc/ssl'.\n\n
+To get started, enter the name of the site e.g. 'example.loc'.\n"
 
-DOMAIN_READ_DESC="Пожалуйста, введите название домена:"
+DOMAIN_READ_DESC="Please enter the domain name:"
 
-QUESTION="Добавляем сайт"
+QUESTION="Adding a site"
 
 # WAARNING
-NOT_GOOD="${red} Сайт, не был добавлен${off}"
-EMPTY_DOMAIN="${red} Вы ввели пустой домен${off}"
-DIR_EXISTS="${red} Такая папка уже есть${off}"
+NOT_GOOD="${red} Site was not added${off}"
+EMPTY_DOMAIN="${red} You entered an empty domain${off}"
+DIR_EXISTS="${red} Such a folder already exists${off}"
 
 OK="$green [OK] $off"
 
-ADD_FOLDER="${green}Создаем директорию${off}"
-ADD_NGINX_CONFIG="${green}Создаем конфиг nginx${off}"
-ADD_SSL="${green}Создаем сертификат${off}"
+ADD_FOLDER="${green}Creating the directory${off}"
+ADD_NGINX_CONFIG="${green}Creating the nginx config${off}"
+ADD_SSL="${green}Creating the certificate${off}"
 
 # # # # # # # # # # # # # # # # # #
 
